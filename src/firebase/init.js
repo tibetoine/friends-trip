dd82b6f4682da12de29bc195f029e02cc8448f32
Antoine ROBERT <tibetoine@gmail.com>
2019-06-23 17:51:08 +0200

    Config Circle Ci and firebase

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,13 @@ import 'firebase/auth'
 
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
-  apiKey: 'AIzaSyCe6CSzOUs6_Ij_VHa6hancdEi6CNBtw7c',
-  authDomain: 'bento-starter.firebaseapp.com',
-  databaseURL: 'https://bento-starter.firebaseio.com',
-  projectId: 'bento-starter',
-  storageBucket: 'bento-starter.appspot.com',
-  messagingSenderId: '458464977217'
+  apiKey: 'AIzaSyDCnSxKfGyVTmsoZ9drx6PSiahA-dXbWAg',
+  authDomain: 'friends-trip-863db.firebaseapp.com',
+  databaseURL: 'https://friends-trip-863db.firebaseio.com',
+  projectId: 'friends-trip-863db',
+  storageBucket: '',
+  messagingSenderId: '96851983867',
+  appId: '1:96851983867:web:134755cb78531293'
 }
 
 firebase.initializeApp(config)
