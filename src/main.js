981e2f201b45b252bbbe25cc95aaf085e01cfdf0
Antoine ROBERT <tibetoine@gmail.com>
2019-08-12 00:16:55 +0200

    Correction Test unitaires qui ne passaient pas avec JEST 24 et remise en place de l'import Vuetify.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 
-import Vuetify from 'vuetify'
+import Vuetify from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
 import App from './App.vue'
 import router from './router'
 import store from './store'
